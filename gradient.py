a7999967734d7f3a00b402201c3979b6021e0919
Han Wang <hanwang@g.harvard.edu>
2025-07-23 00:15:46 -0400

    update

--- gradient.py
+++ gradient.py
@@ -2,7 +2,6 @@ import jax
 import jax.numpy as jnp
 import matplotlib.pyplot as plt
 
-
 x = jnp.array([0.0, 1.0, 2.0, 3.0, 4.0])
 y = jnp.array([1.0, -20.0, 10., -5., -10.])
 
@@ -15,25 +14,33 @@ def DesignSimulation(design, x):
     """
     Reconstruct low dimension representation to high level representation
     """
-    return jnp.polyval(design, x)
+    state = jnp.polyval(design, x)
+    return state
 
-def DesignEvaluation(design, x, y):
+def DesignEvaluation(state, x, y, design):
     """Calculate the values of objectives and constraints"""
-    def loss_fn(design, x, y):
+
+    def loss_fn(state, y):
+        return jnp.mean((state - y)**2)
+    obj_loss = loss_fn(state, y)
+    # Hmmmm
+    def loss_fn_for_gradient(design, x, y):
         preds = jnp.polyval(design, x)
         return jnp.mean((preds - y)**2)
-    val = loss_fn(design, x, y)
-    grads = jax.grad(loss_fn)(design, x, y)
-    return val, grads
+    grads = jax.grad(loss_fn_for_gradient)(design, x, y)
+
+    return obj_loss, grads
 
 design = jnp.zeros(4)
 lr = 1e-4
 epochs = 100000
 for epoch in range(epochs):
-    val, grads = DesignEvaluation(design, x, y)
+    state = DesignSimulation(design, x)
+    obj_loss, grads = DesignEvaluation(state, x, y, design)
     design = DesignSearch(design, grads, lr)
+
     if epoch % 100 == 0:
-        print(jnp.sum(grads), design)
+        print(obj_loss, sum(grads))
     # break if nan or gradient is trivial
     if any(jnp.isnan(grads)) or abs(jnp.sum(grads)) < .01:
         print(epoch)

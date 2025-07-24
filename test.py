0a91304a1f78be2dc29257b0171fc73394d5addc
Han Wang <hanwang@g.harvard.edu>
2025-07-23 20:50:02 -0400

    update

--- test.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-import matplotlib.pyplot as plt
-import numpy as np
-from numpy.polynomial.polynomial import Polynomial
-from scipy.optimize import curve_fit
-
-
-test = Polynomial([1,2,3])
-print(test)

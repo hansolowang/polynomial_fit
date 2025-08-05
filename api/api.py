14fb2522dbcf87539d92e538927ff73626657d1e
Han Wang <yobynah27@gmail.com>
2025-08-05 09:25:22 -0400

    make package

--- api/api.py
+++ api/api.py
@@ -6,6 +6,7 @@ grad = Grad(DesignEvaluation(DesignObjective(DesignSimulation(DesignEmbedding(de
 design = DesignSearch(design, val, grad)
 """
 
+
 # DesignVector = np.ndarray
 # RobotSystem = "high level representation"
 # SimulationStates = "time varying simulation state"
@@ -27,17 +28,26 @@ class DesignEvaluation(ABC):
     def __init__(self, objectives, f_sim):
         self.objectives = objectives
 
-    def val(self, state):
+    def __call__(self, mode):
+        if mode == "val":
+            return self.val()
+        elif mode == "grad":
+            return self.grad()
+        elif mode == "val_grad":
+            return self.val(), self.grad()
+        else:
+            NotImplementedError
+
+    def val(self):
         loss = 0
         for obj in objectives:
             loss += (state[obj.x] - obj.y) ** 2
         return loss
 
-    @abstractmethod
     def grad(self, x, objectives):
-        ...
+        raise NotImplementedError
 
 
 class DesignSearch(ABC):
     def search(self, x, grads, lr):
-         return x - lr * grads
+        return x - lr * grads
--- examples/polyfit.py
+++ examples/polyfit.py
@@ -1,11 +1,10 @@
-from api import *
-
-from dataclasses import dataclass
 import equinox as eqx
 import jax
 import jax.numpy as jnp
 import numpy as np
 import matplotlib.pyplot as plt
+
+from api import *
 
 
 class EmbedQuadraticFunction(DesignEmbedding):
@@ -26,6 +25,7 @@ class EvalQuadraticFunction(DesignEvaluation):
             for obj in objectives:
                 loss += (state[obj.x] - obj.y) ** 2
             return loss
+
         return jax.grad(f)(design)
 
 
@@ -33,16 +33,17 @@ class Objective(eqx.Module):
     x: int = eqx.field(static=True)
     y: float
 
+
 obj0 = Objective(0, 1.0)
 obj1 = Objective(1, -20.0)
 obj2 = Objective(2, 10.0)
-obj3 = Objective(3, -.5)
+obj3 = Objective(3, -0.5)
 obj4 = Objective(4, -10.0)
 # objectives = [obj0, obj1, obj2, obj3, obj4]
 objectives = [obj0, obj1]
 
 
-design = jnp.zeros(3, dtype='float32')
+design = jnp.zeros(3, dtype="float32")
 horizon = np.linspace(0, 5, 6)
 lr = 1e-3
 epochs = 500
@@ -50,8 +51,8 @@ epochs = 500
 design_search = DesignSearch()
 
 for epoch in range(epochs):
-    f = EmbedQuadraticFunction().embed(design)
-    state = SimQuadraticFunction().simulate(f, horizon)
+    f = EmbedQuadraticFunction(design)
+    state = SimQuadraticFunction(f, horizon)
     val = EvalQuadraticFunction().val(state, objectives)
     grads = EvalQuadraticFunction().grad(design, objectives)
     design = design_search.search(design, grads, lr)
@@ -60,11 +61,11 @@ for epoch in range(epochs):
     if epoch % 100 == 0:
         print(val, sum(grads))
     # break if nan or gradient is trivial
-    if any(jnp.isnan(grads)) or abs(jnp.sum(grads)) < .01:
+    if any(jnp.isnan(grads)) or abs(jnp.sum(grads)) < 0.01:
         print(epoch, jnp.sum(grads), design)
         break
 
 xnew = jnp.linspace(horizon[0], horizon[-1], 1000)
-plt.plot([o.x for o in objectives], [o.y for o in objectives], 'bo')
+plt.plot([o.x for o in objectives], [o.y for o in objectives], "bo")
 plt.plot(xnew, jnp.polyval(design, xnew))
 plt.show()
